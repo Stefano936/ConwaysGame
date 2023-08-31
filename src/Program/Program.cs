f687aa2d952156477310eada44041967fbd4e98b
Stefano <stefano.francolino@correo.ucu.edu.uy>
2023-08-31 19:33:57 -0300

    ConwaysGame2

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -6,11 +6,11 @@ namespace ConwaysGame
     class Program {
         static void Main(string[] args) {
             Importador importador = new Importador(TableroPath);
-                Tablero tablero = new Tablero(Importador.LoadTablero());
+                Tablero tablero = new Tablero(importador.LoadTablero());
                 
                 int lines = importador.GetFileLines();
                 Imprimir imprimir = new Imprimir(tablero, lines, lines);
-                Imprimir.Printer();
+                imprimir.Printer();
         }
     }
 }
